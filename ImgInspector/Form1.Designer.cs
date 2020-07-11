dcfbffbe0505f2fddc111a7204df0727f3d04699
benstanfish <benstanfish@hotmail.com>
2020-07-11 16:33:04 -0700

    Initial Push

--- ImgInspector/Form1.Designer.cs
+++ ImgInspector/Form1.Designer.cs
@@ -28,13 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.checkedListBoxImages = new System.Windows.Forms.CheckedListBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonSelectAll = new System.Windows.Forms.Button();
+            this.buttonSelectNone = new System.Windows.Forms.Button();
+            this.pictureBoxPreview = new System.Windows.Forms.PictureBox();
+            this.buttonWriteFile = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.labelList = new System.Windows.Forms.Label();
+            this.labelPreview = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkedListBoxImages
+            // 
+            this.checkedListBoxImages.AllowDrop = true;
+            this.checkedListBoxImages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBoxImages.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.checkedListBoxImages.FormattingEnabled = true;
+            this.checkedListBoxImages.Location = new System.Drawing.Point(129, 30);
+            this.checkedListBoxImages.Name = "checkedListBoxImages";
+            this.checkedListBoxImages.Size = new System.Drawing.Size(901, 410);
+            this.checkedListBoxImages.TabIndex = 0;
+            this.checkedListBoxImages.SelectedIndexChanged += new System.EventHandler(this.checkedListBoxImages_SelectedIndexChanged);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.buttonOpen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonOpen.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonOpen.Location = new System.Drawing.Point(12, 29);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(110, 30);
+            this.buttonOpen.TabIndex = 1;
+            this.buttonOpen.Text = "Open...";
+            this.buttonOpen.UseVisualStyleBackColor = false;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonSelectAll
+            // 
+            this.buttonSelectAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSelectAll.Location = new System.Drawing.Point(13, 96);
+            this.buttonSelectAll.Name = "buttonSelectAll";
+            this.buttonSelectAll.Size = new System.Drawing.Size(110, 30);
+            this.buttonSelectAll.TabIndex = 2;
+            this.buttonSelectAll.Text = "Select All";
+            this.buttonSelectAll.UseVisualStyleBackColor = true;
+            this.buttonSelectAll.Click += new System.EventHandler(this.buttonSelectAll_Click);
+            // 
+            // buttonSelectNone
+            // 
+            this.buttonSelectNone.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSelectNone.Location = new System.Drawing.Point(13, 132);
+            this.buttonSelectNone.Name = "buttonSelectNone";
+            this.buttonSelectNone.Size = new System.Drawing.Size(110, 30);
+            this.buttonSelectNone.TabIndex = 3;
+            this.buttonSelectNone.Text = "Select None";
+            this.buttonSelectNone.UseVisualStyleBackColor = true;
+            this.buttonSelectNone.Click += new System.EventHandler(this.buttonSelectNone_Click);
+            // 
+            // pictureBoxPreview
+            // 
+            this.pictureBoxPreview.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxPreview.Location = new System.Drawing.Point(1037, 30);
+            this.pictureBoxPreview.Name = "pictureBoxPreview";
+            this.pictureBoxPreview.Size = new System.Drawing.Size(199, 148);
+            this.pictureBoxPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxPreview.TabIndex = 4;
+            this.pictureBoxPreview.TabStop = false;
+            // 
+            // buttonWriteFile
+            // 
+            this.buttonWriteFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonWriteFile.BackColor = System.Drawing.Color.LightGreen;
+            this.buttonWriteFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonWriteFile.Location = new System.Drawing.Point(1037, 409);
+            this.buttonWriteFile.Name = "buttonWriteFile";
+            this.buttonWriteFile.Size = new System.Drawing.Size(196, 30);
+            this.buttonWriteFile.TabIndex = 5;
+            this.buttonWriteFile.Text = "Write";
+            this.buttonWriteFile.UseVisualStyleBackColor = false;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonExit.BackColor = System.Drawing.Color.LightSalmon;
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.Location = new System.Drawing.Point(13, 409);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(110, 30);
+            this.buttonExit.TabIndex = 6;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // labelList
+            // 
+            this.labelList.AutoSize = true;
+            this.labelList.Location = new System.Drawing.Point(126, 7);
+            this.labelList.Name = "labelList";
+            this.labelList.Size = new System.Drawing.Size(72, 17);
+            this.labelList.TabIndex = 7;
+            this.labelList.Text = "Image List";
+            // 
+            // labelPreview
+            // 
+            this.labelPreview.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelPreview.AutoSize = true;
+            this.labelPreview.Location = new System.Drawing.Point(1034, 7);
+            this.labelPreview.Name = "labelPreview";
+            this.labelPreview.Size = new System.Drawing.Size(99, 17);
+            this.labelPreview.TabIndex = 8;
+            this.labelPreview.Text = "Image Preview";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1245, 445);
+            this.Controls.Add(this.labelPreview);
+            this.Controls.Add(this.labelList);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonWriteFile);
+            this.Controls.Add(this.pictureBoxPreview);
+            this.Controls.Add(this.buttonSelectNone);
+            this.Controls.Add(this.buttonSelectAll);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.checkedListBoxImages);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Image Inspector v0.0";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBoxImages;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonSelectAll;
+        private System.Windows.Forms.Button buttonSelectNone;
+        private System.Windows.Forms.PictureBox pictureBoxPreview;
+        private System.Windows.Forms.Button buttonWriteFile;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelList;
+        private System.Windows.Forms.Label labelPreview;
     }
 }
 
--- ImgInspector/Form1.cs
+++ ImgInspector/Form1.cs
@@ -16,5 +16,70 @@ namespace ImgInspector
         {
             InitializeComponent();
         }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ClearAll()
+        {
+            checkedListBoxImages.Items.Clear();
+            pictureBoxPreview.InitialImage = null;
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Image files (*.jpg, *.jpeg, *.gif, *.png, *.svg, *.bmp, *.tiff)|*.jpg; *.jpeg; *.gif; *.png; *.svg; *.bmp; *.tiff;)";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.Multiselect = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                checkedListBoxImages.Items.AddRange(openFileDialog1.FileNames);
+                for (int i = 0; i < checkedListBoxImages.Items.Count; i++)
+                {
+                    checkedListBoxImages.SetItemChecked(i, true);
+                }
+            }
+
+        }
+
+        private void SelectAll()
+        {
+            for (int i = 0; i < checkedListBoxImages.Items.Count; i++)
+            {
+                checkedListBoxImages.SetItemChecked(i, true);
+            }
+        }
+
+        private void SelectNone()
+        {
+            while (checkedListBoxImages.CheckedIndices.Count > 0)
+            {
+                checkedListBoxImages.SetItemChecked(checkedListBoxImages.CheckedIndices[0], false);
+            }
+        }
+
+        private void buttonSelectAll_Click(object sender, EventArgs e)
+        {
+            SelectAll();
+        }
+
+        private void buttonSelectNone_Click(object sender, EventArgs e)
+        {
+            SelectNone();
+        }
+
+        private void checkedListBoxImages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBoxPreview.Image = Image.FromFile(checkedListBoxImages.SelectedItem.ToString());
+        }
+
+      
     }
 }
